4e0ddc8977b40e883831b3fbecf33ac4c9ce52be
matteo.simonetti <matteo.simonetti@marconirovereto.it>
2023-11-03 11:09:09 +0100

    non va la breadboard (dio ...)

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("ciao")
+from gpiozero import LED       
+from time import sleep          
+                                
+print("Inizializzazione Led")   
+led = LED(26)                   
+
+print("Inizio lampeggio Led")   
+while True:                      
+    print("Led Acceso")
+    led.on()                   
+    sleep(1)                
+    print("Led Spento")
+    led.off()                  
+    sleep(1)                    
